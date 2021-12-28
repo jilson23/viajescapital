86f2f75df923a0088caf8160f794d06971870a61
jilson jimenez <jilsonjimenez@gmail.com>
2021-12-28 15:21:08 -0500

    Home promovacaciones and routing

--- src/pages/promovacaciones/index.jsx
+++ src/pages/promovacaciones/index.jsx
@@ -3,9 +3,13 @@ import './style.css'
 const Promovacaciones = () => {
     return(
         <div className="Promovacaciones-page">
-            <h2>Promovacaciones</h2>
-            <div className="contenedor">
-                
+            <div className="Promovacaciones-page__titulo">
+                <img src='./SVG/date.svg' alt="" />
+                <h2>Promovacaciones</h2>
+            </div>
+            
+            <div className="Promovacaciones-page__contenedor">
+                adsadasdaddsadad sadasdasdsada dsdada
             </div>
         </div>
     )
