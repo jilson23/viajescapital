f750c0fd792c0871e0bbcb1b8e3ae37c179a6b06
jilson jimenez <jilsonjimenez@gmail.com>
2022-03-02 15:22:57 -0500

    fix bug image warning

--- src/components/advertence/index.jsx
+++ src/components/advertence/index.jsx
@@ -2,7 +2,7 @@ import './style.css';
 const Advertence = ({ content }) => {
   return (
     <div className="Advertence">
-      <img src="./SVG/warning.svg" alt="" />
+      <img src="/SVG/warning.svg" alt="" />
       <p>{content}</p>
     </div>
   );
